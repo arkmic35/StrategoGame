80513a1622dbc198e218fbf57dc5fdf5cfe18353
Arkadiusz <arkmic35@gmail.com>
2018-04-19 11:23:02 +0200

    Dodana aktywność z opcjami gry.

---
+++ app/src/main/java/settings/PreferencesActivity.kt
@@ -0,0 +1,18 @@
+package settings
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.arkmic35.stratego.R
+
+class PreferencesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preferences)
+    }
+
+    fun startGame(view: View) {
+
+    }
+}
